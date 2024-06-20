8cfc1a0af8ac1b7fe46b55b5ef2b0f25469baade
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-06-20 14:04:16 -0600

    refactor: Use environment variable for WhatsApp access token

--- controllers/whatsAppControllers.js
+++ controllers/whatsAppControllers.js
@@ -1,9 +1,10 @@
 // Home Controllers
 // ****************
+require("dotenv").config();
 
 const VerifyToken = (req, res) => {
   try {
-    let accessToken = "987654321";
+    let accessToken = process.env.WA_ACCESS_TOKEN;
     let token = req.query("hub.verify_token");
     let challenge = req.body("hub.challenge");
     if (challenge != null && token != null && token == accessToken) {
