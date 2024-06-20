bb192da56462366a15f4e1c935d5a3f2375f7c4f
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-06-20 14:31:07 -0600

    refactor: Update WhatsAppControllers.js to use dot notation for query parameters

--- controllers/whatsAppControllers.js
+++ controllers/whatsAppControllers.js
@@ -5,8 +5,12 @@ require("dotenv").config();
 const VerifyToken = (req, res) => {
   try {
     let accessToken = process.env.WA_ACCESS_TOKEN;
-    let token = req.query("hub.verify_token");
-    let challenge = req.query("hub.challenge");
+    console.log("accessToken", accessToken);
+    let token = req.query["hub.verify_token"];
+    console.log("token", token);
+    let challenge = req.query["hub.challenge"];
+    console.log("challenge", challenge);
+
     if (challenge != null && token != null && token == accessToken) {
       res.send(challenge);
     } else {
