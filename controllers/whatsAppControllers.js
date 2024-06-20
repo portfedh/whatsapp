347e56be95724e00ef12fd169b24a38a19684a4b
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-06-20 14:10:10 -0600

    refactor: Fix bug in WhatsAppControllers.js

--- controllers/whatsAppControllers.js
+++ controllers/whatsAppControllers.js
@@ -6,7 +6,7 @@ const VerifyToken = (req, res) => {
   try {
     let accessToken = process.env.WA_ACCESS_TOKEN;
     let token = req.query("hub.verify_token");
-    let challenge = req.body("hub.challenge");
+    let challenge = req.query("hub.challenge");
     if (challenge != null && token != null && token == accessToken) {
       res.send(challenge);
     } else {
