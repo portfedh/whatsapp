b967fb2eed046971f8d7b7b8fa6773e84f4fa4fa
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-06-20 13:54:24 -0600

    Add: VerifyToken Method

--- controllers/whatsAppControllers.js
+++ controllers/whatsAppControllers.js
@@ -2,7 +2,19 @@
 // ****************
 
 const VerifyToken = (req, res) => {
-  res.send("Hello Send Message");
+  try {
+    let accessToken = "987654321";
+    let token = req.query("hub.verify_token");
+    let challenge = req.body("hub.challenge");
+    if (challenge != null && token != null && token == accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send();
+  }
 };
 
 const ReceiveMessage = (req, res) => {
