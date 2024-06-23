9efd7fee90bfe19361055bd54f0747a0217277b4
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-06-23 09:41:20 -0600

    refactor: Improve logging in sendMessageWhatsApp function

--- services/whatsAppService.js
+++ services/whatsAppService.js
@@ -1,6 +1,10 @@
 const https = require("https");
 
 function sendMessageWhatsApp(textResponse, number) {
+  console.log("Send message function starting...");
+  console.log("Sending message to: ", number);
+  console.log("Message: ", textResponse);
+
   const data = JSON.stringify({
     messaging_product: "whatsapp",
     recipient_type: "individual",
@@ -11,6 +15,7 @@ function sendMessageWhatsApp(textResponse, number) {
       body: textResponse,
     },
   });
+  console.log("Data Variable: ", data);
 
   const options = {
     host: "graph.facebook.com",
@@ -24,6 +29,8 @@ function sendMessageWhatsApp(textResponse, number) {
         "Bearer EAAGbC7g2ej8BO8tVhAoZBHc7alvODhP7QRrUZBhJlXPmr6RIZAYH2JbUVV6V4bghvkFZC8cxlhDwKDdvgwvlKEzAbKze6j8ABk3YWayLZAa22RViEZAmd5XCNZCoEVuqRP0RxkGgXjDLykdvc2BF4ipZCFHdwUczdJoodjZCXM8mBbl2ZA08cbtpD1uBAqEfTsKB9W66sw1eDpVn2ZBEZBxkgr6Sw0s7OjVPV1lkrXhca2U29tBaLp5AB11J",
     },
   }; // Substitute & make env variable
+
+  console.log("Options Variable: ", options);
 
   const req = https.request(options, (res) => {
     res.on("data", (d) => {
