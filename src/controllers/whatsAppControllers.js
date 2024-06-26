2a7ba586a2a9ffd56cad12605174311270d204d4
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-06-25 22:52:06 -0600

    refactor: Update VerifyToken function to use a custom access token

--- src/controllers/whatsAppControllers.js
+++ src/controllers/whatsAppControllers.js
@@ -1,75 +1,88 @@
 // Home Controllers
 // ****************
 const fs = require("fs");
-// const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
 const whatsAppService = require("../services/whatsAppService");
+const { type } = require("os");
 
+/* 
+VerifyToken:
+Function is only called once
+Requires a webhooks to be set up in the Facebook Developer Console
+- Requires a public url to send the request
+- Requires you paste accessToken there to verify the request
+*/
 const VerifyToken = (req, res) => {
-  // try {
-  //   let accessToken = "679e95b74f7b417c97b40413cdf798d9"; // Make Environment Variable
-  //   let token = req.query["hub.verify_token"];
-  //   let challenge = req.query["hub.challenge"];
-  //   if (challenge != null && token != null && token == accessToken) {
-  //     res.send(challenge);
-  //   } else {
-  //     res.status(400).send({ accessToken, token, challenge });
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(400).send({ err, accessToken, token, challenge });
-  // }
-  res.send("hola Verify Token");
+  try {
+    let accessToken = "AngryTurkeys";
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+    if (challenge != null && token != null && token == accessToken) {
+      // If correct:
+      res.send(`Token verified. Your challenge was: ${challenge}`);
+    } else {
+      // If incorrect:
+      res
+        .status(400)
+        .send(`Token rejected. token: ${token}, challenge: ${challenge}`);
+    }
+  } catch (err) {
+    // If error:
+    console.log(err);
+    res.status(400).send(`error: ${err.message}`);
+  }
 };
 
+/* 
+ReceiveMessage:
+Function is called every time a message is received
+Requires a webhooks to be set up in the Facebook Developer Console
+- Requires you to be subscribed to messages
+- EVENT_RECEIVED is a necessary response to confirm message
+*/
 const ReceiveMessage = (req, res) => {
-  // try {
-  //   let entry = req.body["entry"][0];
-  //   let changes = entry["changes"][0];
-  //   let value = changes["value"];
-  //   let messageObject = value["messages"];
-  //   if (typeof messageObject != "undefined") {
-  //     let messages = messageObject[0];
-  //     let text = getTextUser(messages);
-  //     let number = messages["from"];
-  //     // myConsole.log("messages: ", messages);
-  //     console.log("The whole thing: ", entry);
-  //     console.log("ReceiveMessage/Text: ", text);
-  //     console.log("ReceiveMessage/Number: ", number);
-  //     // myConsole.log(text);
-  //     whatsAppService.sendMessageWhatsApp("el usuario dijo " + text, number);
-  //     console.log("Mensaje enviado");
-  //   }
-  //   res.send("EVENT_RECEIVED");
-  // } catch (err) {
-  //   console.log(err);
-  //   // Necessary to avoid resending the message
-  //   res.send("EVENT_RECEIVED");
-  // }
-  res.send("hola Receive Message");
+  try {
+    let entry = req.body["entry"][0];
+    let changes = entry["changes"][0];
+    let value = changes["value"];
+    let messageObject = value["messages"];
+
+    if (typeof messageObject != "undefined") {
+      // If there is a message:
+      let messages = messageObject[0];
+      let text = getTextUser(messages);
+      myConsole.log("messages: ", messages);
+      myConsole.log("text: ", text);
+    }
+
+    res.send("EVENT_RECEIVED");
+  } catch (err) {
+    myConsole.log("Error: ", err.message); // For Debugging
+    res.send("EVENT_RECEIVED");
+  }
 };
 
 function getTextUser(messages) {
-  let text = "";
+  let text;
   let typeMessage = messages["type"];
-
   if (typeMessage === "text") {
+    // Text
     text = messages["text"]["body"];
   } else if (typeMessage === "interactive") {
+    // Interactive
     let interactiveObject = messages["interactive"];
     let typeInteractive = interactiveObject["type"];
-    // myConsole.log(interactiveObject);
-    console.log("getTextUser/Interactive Object: ", interactiveObject);
     if (typeInteractive === "button_reply") {
+      // Interactive Button pressed
       text = interactiveObject["button_reply"]["title"];
     } else if (typeInteractive === "list_reply") {
+      // Interactive List reply
       text = interactiveObject["list_reply"]["title"];
     } else {
-      // myConsole.log("Sin mensaje");
-      console.log("Sin mensaje");
+      myConsole.log("Sin mensaje");
     }
   } else {
-    // myConsole.log("Sin mensaje");
-    console.log("Sin mensaje");
+    myConsole.log("Sin mensaje");
   }
   return text;
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-// Environment variables
-// =====================
-
 // Express
 // =======
-// Import express
 const express = require("express");
 const app = express();
-// Set template engine to EJS: // Check
-app.set("view engine", "ejs");
-// Serve static files from the 'public' directory
-// app.use(express.static("public"));
-// Enable reading JSON data: // Needed
 app.use(express.json());
-// Enable reading from html elements: // Check
 app.use(express.urlencoded({ extended: true }));
 
 // Routes:
