b481ffdbcc9be2affb5d6878370f36074bd56c63
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-07-01 19:29:50 -0600

    Updated code to  new ChatGPT API

--- src/services/chatGptService.js
+++ src/services/chatGptService.js
@@ -1,21 +1,20 @@
-const { Configuration, OpenAIApi } = require("openai");
+const OpenAI = require("openai");
 require("dotenv").config({ path: __dirname + "/../.env" });
 console.log("TestToken: ", process.env.OPENAI_API_KEY);
 
 async function getMessageChatGPT(text) {
-  const configuration = new Configuration({
+  const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  const openai = new OpenAIApi(configuration);
 
-  const response = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: text }],
     max_tokens: 100,
   });
 
-  if (response.status == 200 && response.data.choices.length > 0) {
-    return response.data.choices[0].message.content;
+  if (response.status == 200 && response.choices.length > 0) {
+    return response.choices[0].message.content;
   } else {
     return null;
   }
